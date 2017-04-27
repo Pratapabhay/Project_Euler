8b79904b139cd28bffb6b963d3d4d758abf7dbd2
abhay <pratapabhay981@gmail.com>
2017-04-27 23:13:10 +0530

    Indentation done

--- MST.java
+++ MST.java
@@ -1,168 +1,119 @@
-/* Project Euler: Minimum Spanning Tree
+/**
+ * Project Euler: Minimal Network
+ * MST.java
+ * Purpose: Finds out maximum saving which can be acheived.
+ *
+ * @version 1.0 04/26/17
+ * @author Abhay Singh
+ */
 
-Problem Statement : Find out maximum saving that can be achieved in a given graph with weighted edges.
-
-	Author : Abhay Singh
-	Designation : Student, IIT Mandi
-	Time Complexity : O(n2)
-	Space Complexity : O(n2)
-
-
-*/
-
-// Importing required libraries
-package minimalNetworks
+package minimalNetworks;
 import java.io.*;
 import java.util.*;
 import java.math.*;
 
-class MST
-{
-	private static String inputFile = "input.txt";
-	private static int verticesNumber = 40;
-	static long total = 0;
+class MST {
 
-	static class Edge
-	{
-		int start,end;
-		long weight;
-		public Edge(int start, int end,long weight)
-		{
-			this.start = start;
-			this.end = end;
-			this.weight = weight;
-		}
-	}
+    private static int verticesNumber = 40;
+    static long total = 0;
 
+    /**
+     * Takes input from the given file and return a list of edges
+     *
+     * @param fileName Input file
+     * @return edges An edge object of Edge class containing all the
+     * edges in the graph.
+     */
+    public LinkedList input(String fileName) {
+        Scanner scanner = null;
+        try {
+            File file = new File(fileName);
+            scanner = new Scanner(file);
+        } catch (Exception e) {
+            System.err.println("Please check that the input file exixts in the current directory");
+            System.exit(-1);
+        }
+        String temp;
+        String[] vertices;
+        LinkedList edges = new LinkedList < Edge > ();
+        for (int i = 0; i < verticesNumber; i++) {
+            temp = scanner.nextLine();
+            vertices = temp.split(",");
 
+            for (int j = 0; j < verticesNumber; j++) {
+                if (!vertices[j].equals("-")) {
+                    Edge current = new Edge(i, j, Long.parseLong(vertices[j]));
+                    edges.add(current);
+                    total = total + current.weight;
 
-	// Union Rank Data-Structure Class 
-	static class UnionRank
-	{
-		int[] setArray;
+                }
 
-	// Create n sets
-		public UnionRank(int n)
-		{
-			setArray = new int[n];
+            }
+        }
+        return edges;
+    }
 
-		// Initial value of -1 represents that the set has no parent or it is the root.
-			for (int i = 0; i < n ; i++) {
-				setArray[i] = -1;
-			}
-		}
+    /**
+     * The main method begins execution of the tests.
+     *
+     * @param args input file.
+     */
 
-		int find(int n)
-		{
-			if(this.setArray[n] == -1)
-				return n;
-			else return(find(this.setArray[n]));
-
-		}
-
-
-		// This union function searches for the parent of vertices in consideration and if set parent of one node to another, if they do not have same parent.
-		void union(int set1, int set2)
-		{
-			int parent1 = this.find(set1);
-			int parent2 = this.find(set2);
-
-			if(parent1 != parent2)
-			{
-				this.setArray[parent1] = parent2;
-			}
-
-		}
-	}
-
-	// Take input from the given file and return a list of edges
-	public LinkedList input(String fileName)
-{	
-		Scanner scanner = null;
-		try{
-			File file = new File(fileName);
-			scanner = new Scanner(file);
-}
-	catch(Exception e)
-{
-	System.err.println("Please check that the input file exixts in the current directory");
-	System.exit(-1);
-}
-		String temp;
-		String[] vertices;
-		LinkedList edges = new LinkedList<Edge>(); 
-		for(int i = 0 ; i < verticesNumber ; i++)
-		{
-			temp = scanner.nextLine();
-			vertices = temp.split(",");	
-
-			for(int j = 0 ; j < verticesNumber ; j++)
-			{
-				if(!vertices[j].equals("-"))
-				{
-					Edge current = new Edge(i,j,Long.parseLong(vertices[j])); 
-					edges.add(current);
-					
-
-					total = total + current.weight;
-
-				}
-
-			}
-		}
-		return edges;
-	}
-
-	public static void main(String[] args)throws Exception {
-		MST object = new MST();
-		if(args.length != 1)
-		{
-			System.err.println("Run the program via following command : \njava MST <input_file>");
-			System.exit(1);
-		}
-		String fileName = args[0];
-		LinkedList edges = object.input(fileName);
-		
-		
-		
-
-		// Sort the input edges according to their weights
-
-		Collections.sort(edges, new Comparator<Edge>()
-		{
-			public int compare(Edge e1,Edge e2)
-			{
-				if(e1.weight >= e2.weight)
-					return 1;
-				else 
-					return -1;
-			}
-				});
+    public static void main(String[] args) throws Exception {
+        MST object = new MST();
+        if (args.length != 1) {
+            System.err.println("Run the program via following command : \njava minimalNetworks/MST <input_file>");
+            System.exit(1);
+        }
+        String fileName = args[0];
+        LinkedList edges = object.input(fileName);
 
 
+        /**
+         * Sort the input edges according to their weights.
+         *
+         * @param edges Object of Edge Class
+         * @param Comparator To compare the elements of Edge object
+         */
 
-		// addedEdges will contain all the edges that are included in the Minimum Spanning Tree
-		LinkedList addedEdges = new LinkedList<Edge>();
-		long sum = 0;
-		UnionRank obj = new UnionRank(verticesNumber);
-		Iterator it = edges.iterator();
-		int addedCount = 0;
-		
-		while(it.hasNext() && addedCount <= (verticesNumber-1))
-		{
-			Edge current = (Edge)it.next();
-			int parent1 = obj.find(current.start);
-			int parent2 = obj.find(current.end);
-			
-			// cycle detection, if parent1 == parent2, then there is a cycle
-			if(parent1 != parent2){
-				addedEdges.add(current);
-				addedCount++;
-				obj.union(parent1,parent2);
-				sum = sum+current.weight;
-			}
-		}
+        Collections.sort(edges, new Comparator < Edge > () {
+            public int compare(Edge e1, Edge e2) {
+                if (e1.weight >= e2.weight) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        });
 
-		System.out.println("Maximum saving achieved:"+(total/2-sum));
-	}
+
+        /**
+         * addedEdges will contain all the edges that are included
+         * in the Minimum Spanning Tree
+         */
+        LinkedList addedEdges = new LinkedList < Edge > ();
+        long sum = 0;
+        UnionRank obj = new UnionRank(verticesNumber);
+        Iterator it = edges.iterator();
+        int addedCount = 0;
+
+        while (it.hasNext() && addedCount <= (verticesNumber - 1)) {
+            Edge current = (Edge) it.next();
+            int parent1 = obj.find(current.start);
+            int parent2 = obj.find(current.end);
+
+            /**
+             * Cycle detection, if parent1 == parent2, then there is
+             * a cycle.
+             */
+            if (parent1 != parent2) {
+                addedEdges.add(current);
+                addedCount++;
+                obj.union(parent1, parent2);
+                sum = sum + current.weight;
+            }
+        }
+
+        System.out.println("Maximum saving achieved:" + (total / 2 - sum));
+    }
 }
